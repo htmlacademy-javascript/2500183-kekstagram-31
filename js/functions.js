3b58ed8a726ca868d1d74096ecebba1f39fb1224
имя фамилия < >
2024-02-19 23:57:03 +0700

    second homework, second change

--- js/functions.js
+++ js/functions.js
@@ -1,22 +1,34 @@
 // Функция для проверки длины строки.
 //Не беспокоился о типах данных аргументов и о том, как должна вести себя функция, если ей были переданы неверные аргументы.
-function checkStringlength(string, maxlength) {
-  return string.length <= maxlength;
+function getMyStr (str, mynumber) {
+
+  return str.length <= mynumber;
 }
+
+getMyStr ('длиннаястрока', 10);
+getMyStr ('длиннаястрока', 30);
+
 //Функция для проверки, является ли строка палиндромом, вариант как по заданию!
 
-function checkStringPolidrom(string) {
+function getStrgPol (string) {
 
-  let strNormalize = string.replaceAll(' ', '').toUpperCase();
+  let strNorm = string.replaceAll(' ', '');
+  strNorm = strNorm.toUpperCase();
   let strReverse = '';
 
-  for (let i = strNormalize.length-1; i >=0; i--) {
-    strReverse+=strNormalize[i];
+  for (let i = strNorm.length - 1; i >= 0; i--) {
+    strReverse += strNorm[i];
   }
 
-  return strNormalize === strReverse;
+  return strNorm === strReverse;
 
 }
+getStrgPol ('дед');
+getStrgPol ('Шалаш');
+getStrgPol ('Шалаш Шалаш Шалаш');
+getStrgPol ('Интересное задание спасибо за проверку');
+
+
 //Функция для проверки, является ли строка палиндромом - мой вариант!
 /*
 function checkStringPolidrom  (str) {
