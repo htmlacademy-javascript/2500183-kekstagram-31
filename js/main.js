4728fba86a59aba95e895a120af5dfc5a61dff37
имя фамилия < >
2024-03-09 00:57:30 +0700

    home work number 7 plus for

--- js/main.js
+++ js/main.js
@@ -1,2 +1,1 @@
 import './photos/thumbnails.js';
-
--- js/photos/thumbnails.js
+++ js/photos/thumbnails.js
@@ -4,14 +4,14 @@ const template = document.querySelector('#picture').content.querySelector('.pict
 const container = document.querySelector('.pictures');
 const fragment = document.createDocumentFragment();
 
-createPhotos.forEach((photos) => { // как избежать стрелочной функции, ведь ругается lint, если сделать по-старой записи
+for (let i = 0; i < createPhotos.length; i++) {
   const thumbnail = template.cloneNode(true);
   const image = thumbnail.querySelector('.picture__img');
 
-  image.src = photos.url;
-  image.description = photos.description;
-  thumbnail.querySelector('.picture__likes').textContent = photos.likes;
-  thumbnail.querySelector('.picture__comments').textContent = photos.comments.length;
+  image.src = createPhotos[i].url;
+  image.description = createPhotos[i].description;
+  thumbnail.querySelector('.picture__likes').textContent = createPhotos[i].likes;
+  thumbnail.querySelector('.picture__comments').textContent = createPhotos[i].comments.length;
   fragment.appendChild(thumbnail);
-});
+}
 container.appendChild(fragment);
