771e581bf3092849187d7f6d932ad54dd6522282
ivan <rineroriner@gmail.com>
2024-04-13 10:59:13 +0300

    добавил загрузку

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import {renderPhotos} from './photos/thumbnails.js';
 import './photos/big-thumbnails.js';
 import {getData} from './photos/api.js';
 import {showErrorMessage} from './photos/error.js';
+import'./photos/upload-image.js';
 
 import {initializeBigPhoto} from './photos/big-thumbnails.js';
 
